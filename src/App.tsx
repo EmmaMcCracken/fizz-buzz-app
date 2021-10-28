f6ef320f48574dd953aa67e52125433e2c36a3c6
Emma <emma.mccracken14@gmail.com>
2021-10-28 13:12:16 +0100

    createdFizzBuzz

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
-import { greet } from "./utils/greet";
+// import { greet } from "./utils/greet";
+import { useState } from "react";
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  const [fizzBuzzArray, setFizzBuzzArray] = useState<(number | string)[]>([1]);
+
+  function addNextElement() {
+    let nextElement: number | string;
+    if ((fizzBuzzArray.length + 1) % 15 === 0) {
+      nextElement = "FizzBuzz";
+    } else if ((fizzBuzzArray.length + 1) % 3 === 0) {
+      nextElement = "Fizz";
+    } else if ((fizzBuzzArray.length + 1) % 5 === 0) {
+      nextElement = "Buzz";
+    } else {
+      nextElement = fizzBuzzArray.length + 1;
+    }
+
+    setFizzBuzzArray([...fizzBuzzArray, nextElement]);
+  }
+  let displayFizzBuzz: (number | string)[];
+  if (fizzBuzzArray.length !== 1) {
+    displayFizzBuzz = [
+      fizzBuzzArray.slice(0, fizzBuzzArray.length - 1).join(", "),
+      " and ",
+      fizzBuzzArray[fizzBuzzArray.length - 1],
+      ".",
+    ];
+  } else {
+    displayFizzBuzz = fizzBuzzArray;
+  }
+
+  return (
+    <>
+      <p>{displayFizzBuzz}</p>
+      <button onClick={addNextElement}>Next</button>
+    </>
+  );
 }
 
 export default App;
