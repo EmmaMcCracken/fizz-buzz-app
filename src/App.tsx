bacf1c6cc3586b7a793cc8549e0dd30495fa9f37
Emma <emma.mccracken14@gmail.com>
2021-10-28 14:14:06 +0100

    usedaternary

--- src/App.tsx
+++ src/App.tsx
@@ -11,21 +11,34 @@ function App(): JSX.Element {
       getFizzBuzzElement(fizzBuzzArray.length + 1),
     ]);
   }
-  let displayFizzBuzz: (number | string)[];
-  if (fizzBuzzArray.length !== 1) {
-    displayFizzBuzz = [
-      fizzBuzzArray.slice(0, fizzBuzzArray.length - 1).join(", "),
-      " and ",
-      fizzBuzzArray[fizzBuzzArray.length - 1],
-      ".",
-    ];
-  } else {
-    displayFizzBuzz = fizzBuzzArray;
-  }
+  // let displayFizzBuzz: (number | string)[];
+  // if (fizzBuzzArray.length !== 1) {
+  //   displayFizzBuzz = [
+  //     fizzBuzzArray.slice(0, fizzBuzzArray.length - 1).join(", "),
+  //     " and ",
+  //     fizzBuzzArray[fizzBuzzArray.length - 1],
+  //     ".",
+  //   ];
+  // } else {
+  //   displayFizzBuzz = fizzBuzzArray;
+  // }
+
+  const fizzBuzzWithAnd = [
+    fizzBuzzArray.slice(0, fizzBuzzArray.length - 1).join(", "),
+    " and ",
+    fizzBuzzArray[fizzBuzzArray.length - 1],
+    ".",
+  ];
 
   return (
     <>
-      <p>{displayFizzBuzz}</p>
+      {/* <p>{fizzBuzzArray.length !== 1 && fizzBuzzWithAnd}</p>
+      <p>{fizzBuzzArray.length === 1 && fizzBuzzArray}</p> */}
+      {fizzBuzzArray.length !== 1 ? (
+        <p>{fizzBuzzWithAnd}</p>
+      ) : (
+        <p>{fizzBuzzArray}</p>
+      )}
       <button onClick={addNextElement}>Next</button>
     </>
   );
--- src/utils/getFizzBuzzElement.test.ts
+++ src/utils/getFizzBuzzElement.test.ts
@@ -1,1 +1,13 @@
-import { getFizzBuzzElement } from "./utils/getFizzBuzzElement";
+import { getFizzBuzzElement } from "./getFizzBuzzElement";
+
+test("Numbers which are not multiples of 3 or 5 remain unchanged", () => {
+  expect(getFizzBuzzElement(1)).toBe(1);
+  expect(getFizzBuzzElement(17)).toBe(17);
+  expect(getFizzBuzzElement(59)).toBe(59);
+});
+
+test("Multiples of 3 are `Fizz`.", () => {
+  expect(getFizzBuzzElement(3)).toBe("Fizz");
+  expect(getFizzBuzzElement(81)).toBe("Fizz");
+  expect(getFizzBuzzElement(6561)).toBe("Fizz");
+});
